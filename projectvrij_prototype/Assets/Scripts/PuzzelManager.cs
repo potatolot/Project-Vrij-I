e66cbbaeea12f47e8de281a4f2913da2df92bba2
Lotte de Jonge <ln.dejonge@outlook.com>
2020-05-28 10:30:56 +0200

    ADDED-PuzzelManager

---
+++ projectvrij_prototype/Assets/Scripts/PuzzelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PuzzelManager : MonoBehaviour
+{
+    // list with puzzleparts
+    [SerializeField] private List<PuzzelPiece> puzzelParts = new List<PuzzelPiece>();
+
+    // When the manager get a trigger
+    private void OnTriggerStay(Collider other)
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = 0; i < puzzelParts.Count; i++)
+            {
+                //Checks whether the puzzle piece is not yet visible
+                if (!puzzelParts[i].isVisible)
+                {
+                    //Makes the puzzle piece visible
+                    puzzelParts[i].SetVisible();
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ projectvrij_prototype/Assets/Scripts/PuzzelPiece.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzelPiece : MonoBehaviour
+{
+    // Holds whether you can see the puzzelpiece
+    private bool visible = false;
+    [HideInInspector] public bool isVisible { get { return visible; } }
+
+
+    //Makes the puzzelpiece visible
+    public void SetVisible()
+    {
+        //Sets the puzzle piece visible
+        visible = true;
+        GetComponent<Renderer>().enabled = true;
+    }
+
+}
